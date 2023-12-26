464e2914e195ca3b136e3e7a37e83e5ba08f410e
vishalkank <kankvishalkank@gmail.com>
2023-12-26 20:27:24 +0530

    create route for edit and delete with its pages

--- app/profile/page.jsx
+++ app/profile/page.jsx
@@ -2,10 +2,13 @@
 import React, { useEffect, useState } from 'react'
 import Profile from '@components/Profile'
 import { useSession } from 'next-auth/react'
+import { useRouter } from 'next/navigation'
 
 const MyProfile = () => {
     const { data: session } = useSession()
     const [posts, setPosts] = useState([])
+    const router = useRouter()
+
     useEffect(() => {
         const fetchPosts = async () => {
           const response = await fetch(`/api/users/${session?.user.id}/posts`);
@@ -16,8 +19,24 @@ const MyProfile = () => {
         if(session?.user.id) fetchPosts();
       }, [])
 
-    const handleEdit = () => {}
-    const handleDelete = () => {}
+    const handleEdit = (post) => {
+      router.push(`/update-prompt?id=${post._id}`)
+    }
+    const handleDelete = async (post) => {
+      const hasConfirmed = confirm("Are you sure you want to delete  this prompt ?");
+      if(hasConfirmed) {
+        try {
+          await fetch(`/api/prompt/${post._id.toString()}`, {
+            method: 'DELETE'
+          })
+          const filteredPosts = posts.filter(p => p._id !== post._id)
+
+          setPosts(filteredPosts);
+        } catch (error) {
+          console.log(error)
+        }
+      }
+    }
     return (
         <Profile
             name={'My'}
